f8bc7de4d3ba8e4c647e905135ebac8334b33335
fish <lunarisfelix02@gmail.com>
2026-09-11 16:17:29 -0400

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1704,6 +1704,10 @@ int do_execveat(int fd, struct filename *filename,
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 
+	#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	#endif
+
 	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 
