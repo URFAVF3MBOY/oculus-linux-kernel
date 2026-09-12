363f982e6fa22cc784e9c3cb53018d0a6b1905cd
fish <lunarisfelix02@gmail.com>
2026-09-12 15:39:37 -0400

    Update rules.c

--- KernelSU-Next/kernel/selinux/rules.c
+++ KernelSU-Next/kernel/selinux/rules.c
@@ -98,7 +98,6 @@ static int apply_kernelsu_rules_fn(void *ptr)
     ksu_type(db, KERNEL_SU_DOMAIN, "domain");
     ksu_permissive(db, KERNEL_SU_DOMAIN);
 	ksu_bound_type(db, KERNEL_SU_DOMAIN, "init");
-	ksu_typeattribute(db, KERNEL_SU_DOMAIN, "mlstrustedsubject");
     ksu_typeattribute(db, KERNEL_SU_DOMAIN, "mlstrustedsubject");
     ksu_typeattribute(db, KERNEL_SU_DOMAIN, "netdomain");
     ksu_typeattribute(db, KERNEL_SU_DOMAIN, "bluetoothdomain");
